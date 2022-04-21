a486a24cbe27f6cd1a887bcb3a4e94740a876fce
Neder Alfonso Fandino Andrade <neder.fandino@decameron.com>
2022-04-20 22:19:33 -0500

    refactor database migration

--- migrations.php
+++ migrations.php
@@ -1,4 +1,6 @@
 <?php
 
+$connection = require "connection.php";
+
 $people_migration_result = @pg_exec($connection, "create table people(id serial, first_name varchar(50), last_name varchar(50))");
 $company_migration_result = @pg_exec($connection, "create table company(id serial, name varchar(50))");
