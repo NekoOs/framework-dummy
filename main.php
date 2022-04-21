66bda20a9871408aae5f42790b3192a270e15de4
Neder Alfonso Fandino Andrade <neder.fandino@decameron.com>
2022-04-20 21:21:58 -0500

    create table people

--- main.php
+++ main.php
@@ -2,4 +2,5 @@
 
 $connection = @pg_connect("host=172.17.0.2 dbname=christian user=postgres password=123456");
 
+$result = pg_exec($connection, "create table people(id serial, first_name varchar(50), last_name varchar(50))");
 
