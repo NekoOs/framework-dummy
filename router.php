3bd2e01f543510bf5489e6fd25e7a5cf97abbc8b
Neder Alfonso Fandino Andrade <neder.fandino@decameron.com>
2022-04-20 23:59:24 -0500

    refactored router using controller and method var

--- router.php
+++ router.php
@@ -6,24 +6,31 @@ use Repositories\Person;
 
 require "migrations.php";
 
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 $command = $_GET['option'];
+
+$array = explode(':', $command);
+$controller = $array[0];
+$method = $array[1];
+
+$routes = [
+    'person' => Repositories\Person::class,
+    'company' => Repositories\Company::class,
+];
+
+$class = $routes[$controller];
+
 
 require "functions.php";
 require "Repositories/Company.php";
 require "Repositories/Person.php";
 
-if ($command == "person:create") {
-    $controller = new Person();
-    $controller->store($_GET);
-} elseif ($command == "person:show") {
-    $controller = new Person();
-    $controller->show($_GET['id']);
-} elseif ($command == "person:update") {
-    $controller = new Person();
-    $controller->update($_GET);
-} elseif ($command == "company:create") {
-    $controller = new Company();
-    $controller->store($_GET);
+$controller = new $class;
+
+if (method_exists($controller, $method)) {
+    $controller->$method($_GET);
 } else {
     echo "The command '$command' not found.".PHP_EOL;
 }
