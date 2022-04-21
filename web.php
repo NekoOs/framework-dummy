f2377a89b37b751b37d5ec3740ec66579a9f2899
Neder Alfonso Fandino Andrade <neder.fandino@decameron.com>
2022-04-20 22:04:01 -0500

    added web interface

---
+++ web.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * execute in console "php -S <address>:<port> <path>"
+ *
+ * @example
+ *         php -S localhost:8000 ./
+ *         php -S 127.0.0.1:80 /var/www/html
+ *         php -S 192.168.60.12:7635 C:\public_html
+ *
+ * php -S localhost:8025
+ */
