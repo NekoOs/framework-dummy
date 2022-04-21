2b462739adeca82ff2ab2e0943e4e42df151b2b4
Neder Alfonso Fandino Andrade <neder.fandino@decameron.com>
2022-04-20 22:15:44 -0500

    import connection var

--- connection.php
+++ connection.php
@@ -1,3 +1,5 @@
 <?php
 
 $connection = @pg_connect("host=172.17.0.2 dbname=christian user=postgres password=123456");
+
+return $connection;
--- web.php
+++ web.php
@@ -11,7 +11,7 @@
  * php -S localhost:8025
  */
 
-require "connection.php";
+$connection = require "connection.php";
 
 $people_migration_result = @pg_exec($connection, "create table people(id serial, first_name varchar(50), last_name varchar(50))");
 $company_migration_result = @pg_exec($connection, "create table company(id serial, name varchar(50))");
