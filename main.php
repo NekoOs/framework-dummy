940ea1b283383dcf9ec28c799d933930babd5078
Neder Alfonso Fandino Andrade <neder.fandino@decameron.com>
2022-04-20 21:35:51 -0500

    add inserts feature

--- main.php
+++ main.php
@@ -7,11 +7,15 @@ $company_migration_result = @pg_exec($connection, "create table company(id seria
 
 /*
  * php main.php "Company Name Example" "Neder" "Fandiño"
- * $argv[0] => "Company Name Example"
- * $argv[1] => "Neder"
- * $argv[2] => "Fandiño"
+ * $argv[0] => "main.php"
+ * $argv[1] => "Company Name Example"
+ * $argv[2] => "Neder"
+ * $argv[3] => "Fandiño"
  */
 
-$company_name = $argv[0];
-$person_first_name = $argv[1];
-$person_last_name = $argv[2];
+$company_name = $argv[1];
+$person_first_name = $argv[2];
+$person_last_name = $argv[3];
+
+pg_exec($connection, "insert into company(name) values ('$company_name')");
+pg_exec($connection, "insert into people(first_name, last_name) values ('$person_first_name', '$person_last_name')");
