b5a4fe163234e9c363b5c4d98e24b120c148c38a
Neder Alfonso Fandino Andrade <neder.fandino@decameron.com>
2022-04-20 22:21:55 -0500

    using logs

--- connection.php
+++ connection.php
@@ -2,4 +2,6 @@
 
 $connection = @pg_connect("host=172.17.0.2 dbname=christian user=postgres password=123456");
 
+file_put_contents('database_log', "new connection create...");
+
 return $connection;
