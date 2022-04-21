07c6ecd073acbc185dd11352a42b47dee3a3dcb5
Neder Alfonso Fandino Andrade <neder.fandino@decameron.com>
2022-04-20 21:25:35 -0500

    create company relation

--- main.php
+++ main.php
@@ -2,5 +2,6 @@
 
 $connection = @pg_connect("host=172.17.0.2 dbname=christian user=postgres password=123456");
 
-$result = @pg_exec($connection, "create table people(id serial, first_name varchar(50), last_name varchar(50))");
+$people_migration_result = @pg_exec($connection, "create table people(id serial, first_name varchar(50), last_name varchar(50))");
+$company_migration_result = @pg_exec($connection, "create table company(id serial, name varchar(50))");
 
